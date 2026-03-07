cf02b6db3babf46f87b7bc873dbb861f3d8d25ae
msdpramod <kuppapramod@gmail.com>
2026-03-07 19:18:10 +0530

    added CreationTimestamp and UpdateTimestamp

--- src/main/java/com/flipkart/flipkartapplication/models/User.java
+++ src/main/java/com/flipkart/flipkartapplication/models/User.java
@@ -3,8 +3,12 @@ package com.flipkart.flipkartapplication.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedDate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
@@ -35,4 +39,10 @@ public class User {
     @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "address_id", referencedColumnName = "id")
     private Address address;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
